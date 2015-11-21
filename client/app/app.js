e6472f4d7aee601c10042c9902eb8b3a10ef7a7c
jpagand <jordane.pagand@epfl.ch>
2015-11-21 13:43:49 +0100

    Fixed header, menu & image

--- client/app/app.js
+++ client/app/app.js
@@ -10,6 +10,7 @@ angular.module('rtsHackdaysApp', [
   'com.2fdevs.videogular.plugins.controls',
   'com.2fdevs.videogular.plugins.overlayplay',
   'com.2fdevs.videogular.plugins.poster',
+  'duScroll'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
